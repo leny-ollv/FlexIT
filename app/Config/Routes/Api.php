a77a7aa10c6143161fb55263468faf4491bab7af
lolliveau <lenyolliveau@gmail.com>
2026-04-09 09:18:50 +0200

    Modifs API

--- app/Config/Routes/Api.php
+++ app/Config/Routes/Api.php
@@ -10,6 +10,7 @@ $routes->group('api',['namespace' => 'App\Controllers\Api'],function($routes){
     });
     $routes->group('programs', ['filter' => 'apitoken'], function($routes){
         $routes->get('all', 'Program::index');
+        $routes->post('create', 'Program::create');
         $routes->get('user/(:num)', 'Program::showall/$1');
         $routes->get('(:num)', 'Program::show/$1');
         $routes->post('delete', 'Program::delete');
